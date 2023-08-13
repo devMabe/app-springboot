2ed09933b4b114abdbff620bdf14cc1e3936b643
Mateo Beltran <matymatbeltran@hotmail.com>
2023-08-13 17:46:30 -0500

    eureka server

--- bike-service/src/main/java/com/app/bikeservice/BikeServiceApplication.java
+++ bike-service/src/main/java/com/app/bikeservice/BikeServiceApplication.java
@@ -2,8 +2,10 @@ package com.app.bikeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BikeServiceApplication {
 
 	public static void main(String[] args) {
